7c7f046c5ebd475346dd7eb8744212add1b62916
2252408699 <49366744+2252408699@users.noreply.github.com>
2023-07-07 14:26:10 +0800

    Add files via upload

--- pages/setup/language.vue
+++ pages/setup/language.vue
@@ -1,16 +1,16 @@
 <template>
 	<view>
+		<u-navbar title="切换语言" back-icon-color="#196ed9"></u-navbar>
 		<u-cell-group title="自动选择" :title-style="{fontSize: '28rpx'}">
 			<u-cell-item title="系统语言" :arrow="false" @click="languageClick">
 				<u-icon slot="right-icon" v-if="!curr" name="checkmark"></u-icon>
 			</u-cell-item>
 		</u-cell-group>
-		
+
 		<view class="u-m-t-40">
 			<u-cell-group title="自动选择" :title-style="{fontSize: '28rpx'}">
-				<u-cell-item v-for="(item, index) in language" 
-					@click="languageClick(index)"
-					:title="item" :arrow="false">
+				<u-cell-item v-for="(item, index) in language" @click="languageClick(index)" :title="item"
+					:arrow="false">
 					<u-icon v-if="index == curr" slot="right-icon" name="checkmark"></u-icon>
 				</u-cell-item>
 			</u-cell-group>
@@ -19,10 +19,11 @@
 </template>
 
 <script>
-	import { mapState } from 'vuex';
+	import {
+		mapState
+	} from 'vuex';
 	export default {
-		computed: {
-		},
+		computed: {},
 		data() {
 			return {
 				index: null,
@@ -37,18 +38,18 @@
 		onShow() {
 			this.curr = uni.getStorageSync('currLanguage')
 		},
-		methods:{
-			languageClick(index){
+		methods: {
+			languageClick(index) {
 				let language = index
 				this.curr = index
-				
-				if(!index){
+
+				if (!index) {
 					language = uni.getStorageSync("localeLan")
 				}
 				uni.setStorageSync('currLanguage', index)
 				uni.setLocale(language)
 				this.$i18n.locale = language;
-				
+
 				this.$utils.handleReLaunch("/pages/tabBar/index")
 			}
 		}
@@ -56,7 +57,7 @@
 </script>
 
 <style lang="scss">
-	/deep/ .u-cell{
+	/deep/ .u-cell {
 		padding: 20rpx !important;
 	}
 </style>
